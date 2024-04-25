dc6e7faafdbc3b10dd504ee94cbcbbd55dcdd6a0
barleysack <hookypooky@naver.com>
2024-04-26 01:23:08 +0900

    ptdq_lstm, why...

--- torch-quantization-tutorial/ptq/lstm_ptdq.py
+++ torch-quantization-tutorial/ptq/lstm_ptdq.py
@@ -112,7 +112,6 @@ class LSTMModel(nn.Module):
         last_state = out[:, -1, :]
         out = self.fc(last_state)
         out = self.activation(out)
-
         
         return out
 
@@ -191,10 +190,47 @@ plt.savefig("./res_img/lstm_before_qt.png")
 print("MAPE: ",mape )
 
 
+class LSTMModel(nn.Module):
+    """Container module with an encoder, a recurrent module, and a decoder."""
 
+    def __init__(self, ntoken, ninp, nhid, nlayers, dropout=0.5):
+        super(LSTMModel, self).__init__()
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        self.rnn = nn.LSTM(ninp, nhid, nlayers, dropout=dropout)
+        self.decoder = nn.Linear(nhid, ntoken)
+
+        self.init_weights()
+
+        self.nhid = nhid
+        self.nlayers = nlayers
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, hidden):
+        emb = self.drop(self.encoder(input))
+        output, hidden = self.rnn(emb, hidden)
+        output = self.drop(output)
+        decoded = self.decoder(output)
+        return decoded, hidden
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters())
+        return (weight.new_zeros(self.nlayers, bsz, self.nhid),
+                weight.new_zeros(self.nlayers, bsz, self.nhid))
+
+# test_model = LSTMModel(10, 10, 10, 10) # 이건 예시인데, 잘 되더라.
 
 # model.qconfig = torch.ao.quantization.default_qconfig
-
+quantized_model = torch.quantization.quantize_dynamic(
+    model, {nn.LSTM, nn.Linear}, dtype=torch.qint8
+)
+print(quantized_model)
+exit()
 model_32_prepared = torch.quantization.prepare(model)
 model_32_prepared.d_device = torch.device("cpu")
 
